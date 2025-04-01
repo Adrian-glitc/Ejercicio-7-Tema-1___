f2a667259c0c4e0ca3192466a931b0d5d84af2b3
Adrian-glitc <amadridm@student.universidadunie.com>
2025-04-01 18:16:01 +0200

    cambios

---
+++ Inventario.py
@@ -0,0 +1,8 @@
+class Inventario:
+    def __init__(self):
+        self.items = []
+
+    def agregar_item(self, item):
+        if item in self.items:
+            raise ValueError(f"El ítem '{item}' ya está en el inventario.")
+        self.items.append(item)
--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-class Inventario:
-    def __init__(self):
-        self.items = []
-
-    def agregar_item(self, item):
-        if item in self.items:
-            raise ValueError(f"El ítem '{item}' ya está en el inventario.")
-        self.items.append(item)
+from Inventario import Inventario
 
 # Ejemplo de uso
 if __name__ == "__main__":
